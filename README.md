b6320ad653c755d438737e9890b3f18a3551840b
Ronald Farrer <canutethegreat@gmail.com>
2015-05-21 12:07:34 -0700

    First upload of actual files.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # zab-smartmon
+Zabbix S.M.A.R.T. Monitor
+
+Description:
+A hard drive S.M.A.R.T. data monitor for Zabbix.
+
+Features:
+Auto discovery of /dev/sdX drives
+Will trigger an alert on monitored disk SMART attributes
+Drives are probed every 10800 seconds (3 hours)
+
+Install:
+1. Make sure smartctl (smartmontools) is installed
+2. Add these three lines to zabbix agent config 
+	UserParameter=smartmon.discovery,sudo /usr/local/bin/smartmon.sh -D
+	UserParameter=smartmon.value[*],sudo /usr/local/bin/smartmon.sh -d $1.$2 -v $3
+	UserParameter=smartmon.health[*],sudo /usr/local/bin/smartmon.sh -d $1.$2 -t
+3. Import smart_monitor.xml on Zabbix Monitoring Server
+4. Manually add any RAID devices to smartmon.sh (see example within)
+5. Make sure zabbix can sudo (to run smartctl)
+
+Common Problems:
+If you get an error similar to "not a valid JSON query" under discovery, you
+probably have an extra comma in your list line of disk discovery in
+smartmon.sh. The last line cannot have a comma.
+
+If all of the SMART values show as "not supported" you most likely need to
+install smartctl on the monitored machine.
+
+If you get alerts that zabbix cannot sudo, you need to give zabbix the ability
+to sudo on the monitored machine. A simple 'echo "zabbix ALL=(ALL) 
+NOPASSWD:ALL" > /etc/sudoers.d/zabbix' should do the trick.
+
+If a drive is missing from the listing, make sure it is not being removed by
+grep in the discovery section of smartmon.sh or if it is RAID make sure it is
+manually listed in same file.
+
+Some drives only give Temperature_Celsius, some only give
+Airflow_Temperature_Cel, and some give both. So in smartmon.xml we look for
+Temperature_Cel (which will catch both) and in smartmon.sh we only take the
+first output. On drives that provide both temperatures there is no guarantee
+which one will show up first. This should only be a minor problem and is the
+result of S.M.A.R.T not being an official set of standards.
+
+Special Thanks:
+Based on works by Kevin Druelle (kdruelle on github)
+
---
+++ smartmon.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+version=0.34
+
+while getopts ":Dd:v:t" optname
+do
+    case "$optname" in
+    "D")
+        # Disk Discovery 
+	#
+	echo "{"
+        echo "\"data\":["
+	# RAID devices
+	#
+	# Manually add hardware RAID or other devices here NOTE: make sure the short name matches it's /dev/... part!
+	# The DISKTYPE portion is what smartctl uses. In the first example it is a highpoint card (hpt), fourth device/disk (1/4) and on the first array
+	#Example: echo "    {\"{#RAIDDEV}\":\"3WARE\",\"{#DISKNAME}\":\"/dev/twa0\",\"{#DISKTYPE}\":\"3ware,0\",\"{#SHORTDISKNAME}\":\"twa0\"},"
+	# Below is an example of a four disk HighPoint RAID array that is accessible via /dev/sdb
+	echo "    {\"{#DISKNAME}\":\"/dev/sdb\",\"{#DISKTYPE}\":\"hpt,1/1/1\",\"{#SHORTDISKNAME}\":\"sdbone\"},"
+	echo "    {\"{#DISKNAME}\":\"/dev/sdb\",\"{#DISKTYPE}\":\"hpt,1/2/1\",\"{#SHORTDISKNAME}\":\"sdbtwo\"},"
+	echo "    {\"{#DISKNAME}\":\"/dev/sdb\",\"{#DISKTYPE}\":\"hpt,1/3/1\",\"{#SHORTDISKNAME}\":\"sdbthree\"},"
+	echo "    {\"{#DISKNAME}\":\"/dev/sdb\",\"{#DISKTYPE}\":\"hpt,1/4/1\",\"{#SHORTDISKNAME}\":\"sdbfour\"},"
+	#
+	# TODO: add any drives listed above in this list to keep it from being rediscovered below in the form of: "sda|sdb|sdc"
+	#
+	IgnoreDisks="sdb"
+	#
+	# Autodiscovery of standard /dev/sdX drives
+	#
+        disks=$(ls -l /dev/disk/by-id/ | grep -v part | grep -v total | grep -vE '('$IgnoreDisks')' | grep -vE 'sr[0-9]' | sed 's/.*..\///' | sed 's/^/\/dev\//' | sort | uniq)
+        typeset -i nbLines
+        typeset -i cntLines=0
+        nbLines=$(ls -l /dev/disk/by-id/ | grep -v part | grep -v total | grep -vE '('$IgnoreDisks')' | grep -vE 'sr[0-9]' | sed 's/.*..\///' | sed 's/^/\/dev\//' | sort | uniq | wc -l)
+        for disk in $disks
+        do
+            cntLines=${cntLines}+1
+            if [ ${cntLines} -eq ${nbLines} ]; then
+                echo "    {\"{#DISKNAME}\":\"$disk\",\"{#DISKTYPE}\":\"sat\",\"{#SHORTDISKNAME}\":\"${disk:5}\"}"
+            else
+                echo "    {\"{#DISKNAME}\":\"$disk\",\"{#DISKTYPE}\":\"sat\",\"{#SHORTDISKNAME}\":\"${disk:5}\"},"
+            fi
+        done
+        echo "]"
+        echo "}"
+        exit 0
+    ;;
+    "d")
+        # Which device and type are we working with when called
+	DeviceType=$(/bin/echo $OPTARG | /usr/bin/awk -F "." '{print $1;}')
+	Device=$(/bin/echo $OPTARG | /usr/bin/awk -F "." '{print $2;}')
+    ;;
+    "v")
+        # Which value to get for the device
+        /usr/sbin/smartctl -d $DeviceType -A $Device | grep $OPTARG | awk '{print $10}' | head -1
+        exit 0
+    ;;
+    "t") 
+    	# Get overall SMART health information: usually "PASSED" if all is well
+       	/usr/sbin/smartctl -d $DeviceType -a $Device | grep "SMART overall-health" | cut -d " " -f 6
+        exit 0
+    ;;
+    "?")
+        echo "Unknown option $OPTARG"
+        exit 1
+    ;;
+    ":")
+        echo "No argument value for option $OPTARG"
+        exit 1
+    ;;
+    *)
+        # This should not occur!
+        echo "ERROR on `hostname` in $0"
+        exit 1
+    ;;
+    esac
+done
